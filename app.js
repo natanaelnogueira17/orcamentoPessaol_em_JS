f712a0016fcdc7aac9dabb07583b0837d7773375
natanael nogueira <natanaelnogueira019@gmail.com>
2023-07-27 14:54:23 -0300

    criando o objeto bd e salando no storage com ids diferente

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+//classe despesas
 class Despesa{
     constructor(ano, mes, dia, tipo, valor, descricao){
        this.ano = ano
@@ -10,6 +11,34 @@ class Despesa{
 
 }
 
+//classe Bd
+class Bd{
+
+    constructor(){
+        let id = localStorage.getItem('id')
+        if(id === null){
+            localStorage.setItem('id', 0)
+        }
+    }
+
+
+    getProximoId(){
+        let proximoId = localStorage.getItem('id')
+        return parseInt(proximoId)+1
+    }
+
+    gravar (d){        
+       let id =  this.getProximoId()
+        localStorage.setItem(id, JSON.stringify(d))
+        localStorage.setItem('id', id)
+    }
+    
+}
+
+let bd = new Bd();
+
+
+//fora da classe
 function cadastrarDespesa(){
     let dia = document.getElementById('dia');
     let mes = document.getElementById('mes');
@@ -18,10 +47,7 @@ function cadastrarDespesa(){
     let descricao = document.getElementById('descricao');
     let valor = document.getElementById('valor');
     let despesa = new Despesa(ano.value, mes.value, dia.value, tipo.value, valor.value, descricao.value);
-    gravar(despesa);
+    bd.gravar(despesa); //interessante que nao importou nada pra usar aqui, apenas chamou com  classe
 }
 
-function gravar (d){
-    localStorage.setItem('despesa', JSON.stringify(d))
-}
-
+ 
