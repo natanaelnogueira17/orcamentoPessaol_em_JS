f4e9ef45b2408d06dd359fafcaaf43c32843835f
natanael nogueira <natanaelnogueira019@gmail.com>
2023-07-27 13:22:12 -0300

    criando a classe despesa

--- app.js
+++ app.js
@@ -1,3 +1,16 @@
+class Despesa{
+    constructor(ano, mes, dia, tipo, valor, descricao){
+       this.ano = ano
+       this.mes = mes
+       this.dia = dia
+       this.tipo = tipo
+       this.descricao = descricao
+       this.valor = valor
+    }
+
+}
+
+
 function cadastrarDespesa(){
     let dia = document.getElementById('dia');
     let mes = document.getElementById('mes');
@@ -5,7 +18,10 @@ function cadastrarDespesa(){
     let tipo = document.getElementById('tipo');
     let descricao = document.getElementById('descricao');
     let valor = document.getElementById('valor');
+    let despesa = new Despesa(ano.value, mes.value, dia.value, tipo.value, valor.value, descricao.value);
+    console.log(despesa)
 
-    console.log(dia.value, mes.value, ano.value, tipo.value, descricao.value, valor.value)
+
 
 }
+
