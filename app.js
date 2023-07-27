a164bb0bf8a0cd4bd01d4c1c640580c601e6f127
natanael nogueira <natanaelnogueira019@gmail.com>
2023-07-27 13:05:18 -0300

    first commit

---
+++ app.js
@@ -0,0 +1,11 @@
+function cadastrarDespesa(){
+    let dia = document.getElementById('dia');
+    let mes = document.getElementById('mes');
+    let ano = document.getElementById('ano');
+    let tipo = document.getElementById('tipo');
+    let descricao = document.getElementById('descricao');
+    let valor = document.getElementById('valor');
+
+    console.log(dia.value, mes.value, ano.value, tipo.value, descricao.value, valor.value)
+
+}
